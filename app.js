ad3707ec9c5f66b31c8a2aba8200ffc14b3a567b
Sylvie-B <unsolo@hotmail.fr>
2020-12-15 16:09:04 +0100

    ouvrir porte affichage color

--- app.js
+++ app.js
@@ -23,7 +23,6 @@ let ford = {
     }
 }
 
-
 /**
  * Votre code commence ici !
  */
@@ -31,10 +30,18 @@ let voitures = {
     garage: 'concession',
     lieu: 'Fourmies',
     // Dans un tableau, ajouter les différentes voitures que vous possédez.
-    collection: [], // Vos voitures dans cette propriété !
+    collection: [peugeot, citroen, ford], // Vos voitures dans cette propriété !
 }
 
 // FIXME Parcourrez le tableau dans lequel vous avez placé vos objets.
-// TODO ==> invoquez la méthode ouvrirPorte pour chaque voiture de la collection.
+for (let i =0 ; i < voitures.collection.length ; i++){
+    // TODO ==> invoquez la méthode ouvrirPorte pour chaque voiture de la collection.
+    voitures.collection[i].ouvrirPorte();
+}
 
-
+let newPara = document.getElementById('newPara');
+let carColor = document.createElement('span');
+for (let i =0 ; i < voitures.collection.length ; i++){
+    carColor.innerHTML += voitures.collection[i].color + " ";
+    newPara.appendChild(carColor);
+}
